5d89e6ef23d52ebe04c2f72ddb328dd6478bb907
David Conway <dconway1985@gmail.com>
2016-04-16 17:50:31 -0400

    dealt with non-flipped y axis

--- core/src/com/mice/SceneGraph.java
+++ core/src/com/mice/SceneGraph.java
@@ -13,35 +13,39 @@ public class SceneGraph {
 
 	private static final Texture reds = new Texture("redmice.png");
 	private static final Texture blues = new Texture("bluemice.png");
-	private static final Texture cheese = new Texture("cheese.png");
-
+	private static final Texture cheeseTex = new Texture("cheese.png");
+	
+	private static final TextureRegion cheese = new TextureRegion(cheeseTex, 0, 0, 25, 25);
 	private static final TextureRegion redStand = new TextureRegion(reds, 0, 0, 25, 25);
 	private static final TextureRegion blueStand = new TextureRegion(blues, 0, 0, 25, 25);
 
 	public SceneGraph() {
 	}
-
+	
+	public static int flipY(CheeseGrid grid, int y) {
+		return Math.abs(grid.height() - y - 1);
+	}
+	
 	public static void drawGrid(CheeseGrid grid, SpriteBatch batch) {
 		for (int x = 0; x < grid.width(); x++) {
 			for (int y = 0; y < grid.height(); y++) {
 				Block block = grid.get(x, y);
-
-				/**
-				 * Note to self: fillgrid() put blue mice on the red side --
-				 * WTF?? Also red mice in column 0
-				 */
+				
+				TextureRegion region = null;
+				int _y = flipY(grid, y);
 
 				if (block.isRedMouse()) {
-					batch.draw(redStand, x * BLOCK_SIZE, y * BLOCK_SIZE + 10);
+					region = redStand;
 				} else if (block.isBlueMouse()) {
-					batch.draw(blueStand, x * BLOCK_SIZE, y * BLOCK_SIZE + 8);
+					region = blueStand;
 				} else if (block.isCheese()) {
-					batch.draw(cheese, x * BLOCK_SIZE, y * BLOCK_SIZE);
+					region = cheese;
 				} else {
 					/** no need to draw empty blocks */
 					continue;
 				}
-
+				
+				batch.draw(region, x * BLOCK_SIZE, _y * BLOCK_SIZE);
 			}
 		}
 	}
