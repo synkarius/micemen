9ad05e95dc172ed310ba1f1bc94ed5cd493f5a94
David Conway <dconway1985@gmail.com>
2016-05-15 17:26:26 -0400

    fixes #6, blue score offset

--- core/src/graphical/SceneGraph.java
+++ core/src/graphical/SceneGraph.java
@@ -22,7 +22,7 @@ public class SceneGraph {
     public static final float BLOCKER_TOP        = 55, BLOCKER_BOTTOM = 100;
     public static final float POLE_OFFSET_X      = (float) 7.5;
     public static final int   POLE_HEIGHT_Y      = 50;
-    public static final int   RED_SCORE_OFFSET_X = X_OFFSET + 5, BLUE_SCORE_OFFSET_X = (int) (WIDTH - X_OFFSET - 20);
+    public static final int   RED_SCORE_OFFSET_X = X_OFFSET + 5, BLUE_SCORE_OFFSET_X = (int) (WIDTH - X_OFFSET - 25);
     public static final int   MENU_TEXT_X_OFFSET = X_OFFSET + 30,
             MENU_MESSAGE_X_OFFSET = (int) (WIDTH - X_OFFSET - 170);
     public static final int   MENU_TEXT_Y        = 20;
